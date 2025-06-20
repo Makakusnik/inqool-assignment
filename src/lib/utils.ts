94b8b0cf2d7d9771a1c0a2162dc409f08953d1d3
Marek Fodor <fodormarek5@gmail.com>
2025-06-20 12:54:38 +0200

    feat: Add data transform utility function.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formToFilterData(formData: object) {
+  return Object.entries(formData).map(([key, value]) => ({
+    id: key,
+    value: value,
+  }));
 }
