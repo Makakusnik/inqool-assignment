39cf8d04aa4220859da7969d615bd38a60f17873
Marek Fodor <fodormarek5@gmail.com>
2025-06-20 13:17:21 +0200

    feat: Add simple navigation and footer to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,7 @@ const geistSans = Geist({
 
 export const metadata: Metadata = {
   title: "InQool | Assignment",
-  description:
-    "A simple Next.js application created as solution to interview assignment at InQool.",
+  description: "A simple Next.js application created as solution to interview assignment at InQool.",
 };
 
 export default function RootLayout({
@@ -21,8 +20,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} antialiased`}>
+      <body className={`${geistSans.variable} antialiased dark`}>
+        <nav className="flex flex-1 justify-center items-center h-12 gap-x-4">
+          <a className="h-fit px-3 py-1.5 border-b border-white/35 hover:border-white/70 active:border-emerald-400 active:text-emerald-400" href="/users">
+            users
+          </a>
+          <a className="h-fit px-3 py-1.5 border-b border-white/35 hover:border-white/70 active:border-emerald-400 active:text-emerald-400" href="/animals">
+            animals
+          </a>
+        </nav>
         <Providers>{children}</Providers>
+        <footer className="row-start-3 flex flex-wrap items-center justify-center h-12">
+          <p>
+            Created by&nbsp;
+            <a className="text-emerald-500 hover:text-emerald-400" href="https://marekus.eu">
+              Marek Fodor
+            </a>
+          </p>
+        </footer>
       </body>
     </html>
   );
