38064e18eff10647a469a4e88df6ca1668ad2932
Marek Fodor <fodormarek5@gmail.com>
2025-06-21 14:26:08 +0200

    refactor: Refine animal schema and types.

--- src/schemas/animal.ts
+++ src/schemas/animal.ts
@@ -1,17 +1,24 @@
 import { z } from "zod";
 
 export const animalTypeFieldSchema = z.enum(["cat", "dog", "other"], {
-  message:
-    'Animal type is required. Possible options are "Cat", "Dog", "Other".',
+  message: 'Animal type is required. Possible options are "Cat", "Dog", "Other".',
 });
 
 export const animalSchema = z.object({
   id: z.string(),
-  name: z
-    .string({ message: "Name is required." })
-    .min(2, { message: "Name must be at least 2 character long." }),
+  name: z.string({ message: "Name is required." }).min(2, { message: "Name must be at least 2 character long." }),
   type: animalTypeFieldSchema,
-  age: z.number(),
+  age: z
+    .string({ message: "Age is required." })
+    .transform((val) => {
+      if (val === "") {
+        return NaN;
+      }
+      const num = Number(val);
+
+      return isFinite(num) ? num : NaN;
+    })
+    .refine((val) => !isNaN(val) && val >= 0, { message: "Age must be a positive number." }),
 });
 
 export const animalCreateSchema = animalSchema.omit({ id: true });
--- src/types/animal.ts
+++ src/types/animal.ts
@@ -1,10 +1,5 @@
 import { z } from "zod";
-import {
-  animalTypeFieldSchema,
-  animalCreateSchema,
-  animalSchema,
-  animalUpdateSchema,
-} from "@/schemas/animal";
+import { animalTypeFieldSchema, animalCreateSchema, animalSchema, animalUpdateSchema } from "@/schemas/animal";
 
 export type AnimalType = z.infer<typeof animalTypeFieldSchema>;
 
@@ -13,3 +8,9 @@ export type AnimalList = Animal[];
 
 export type AnimalCreate = z.infer<typeof animalCreateSchema>;
 export type AnimalUpdate = z.infer<typeof animalUpdateSchema>;
+
+export type AnimalCreateFormInput = z.input<typeof animalCreateSchema>;
+export type AnimalCreateFormOutput = z.output<typeof animalCreateSchema>;
+
+export type AnimalUpdateFormInput = z.input<typeof animalUpdateSchema>;
+export type AnimalUpdateFormOutput = z.output<typeof animalUpdateSchema>;
