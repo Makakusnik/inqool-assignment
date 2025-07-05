110df36446bf64ae3890d79ef2b2a18714e098c3
Marek Fodor <fodormarek5@gmail.com>
2025-07-05 14:39:38 +0200

    refactor: Improv filter types and data transform utility.

--- src/types/filter.ts
+++ src/types/filter.ts
@@ -3,3 +3,12 @@ import { z } from "zod";
 
 export type UserFilter = z.infer<typeof userFilterSchema>;
 export type AnimalFilter = z.infer<typeof animalFilterSchema>;
+
+export type FilterInputObject = {
+  [key: string]: string | number | undefined | null;
+};
+
+export type FilterDataItem<TKey extends string> = {
+  key: TKey;
+  value: string;
+};
